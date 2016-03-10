9c48660cb9f3dc5195fc230ba70ce9a086ac22a8
courses.learner@gmail.com <courses.learner@gmail.com>
2016-03-10 14:28:20 +0300

    Добавил вьюмодел для главного окна

---
+++ Сalorie-Сounter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Сalorie_Сounter.DataBase;
+
+namespace Calorie_Counter.ViewModels
+{
+    class MainWindowViewModel
+    {
+        private DateTime _date;
+        public DateTime Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+            }
+        }
+        private Dictionary<string, List<Dish>> _dailyHistory;
+        public Dictionary<string, List<Dish>> DailyHistory
+        {
+            get
+            {
+                return _dailyHistory;
+            }
+            set
+            {
+                _dailyHistory = value;
+            }
+        }
+        DataBaseRepository _repo = new DataBaseRepository();
+
+        public void GetDailyInfo(DateTime date)
+        {
+            DailyHistory = _repo.GetDailyHistoryData(date); 
+        }
+    }
+}
