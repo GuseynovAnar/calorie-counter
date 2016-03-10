3c2e36b9d166bc37c1192965cbc55096feb4c361
courses.learner@gmail.com <courses.learner@gmail.com>
2016-03-10 15:11:55 +0300

    Добавил NewItemWindowViewModel

---
+++ Сalorie-Сounter/ViewModels/NewItemWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Сalorie_Сounter.DataBase;
+
+namespace Calorie_Counter.ViewModels
+{
+    class NewItemWindowViewModel
+    {
+        DataBaseRepository _repo = new DataBaseRepository(); // vs ctor
+        ObservableCollection<Category> _categories;
+        public ObservableCollection<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+            set
+            {
+                _categories = value;
+            }
+        }
+
+        public NewItemWindowViewModel()
+        {
+            Categories = new ObservableCollection<Category>(_repo.GetCategories());
+        }
+         
+        public void AddNewItem(Dish dish)
+        {
+            _repo.AddDish(dish);
+        }
+    }
+}
