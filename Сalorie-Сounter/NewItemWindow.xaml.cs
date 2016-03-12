469e3b306b0574c29edb3e2c85930f457a81584a
muangelina@yandex.ru <muangelina@yandex.ru>
2016-03-12 03:05:55 +0300

    Сделала страницу с созданием новых блюд

--- Сalorie-Сounter/NewItemWindow.xaml.cs
+++ Сalorie-Сounter/NewItemWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calorie_Counter.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,17 +12,39 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Сalorie_Сounter.DataBase;
 
 namespace Сalorie_Сounter
 {
-    /// <summary>
-    /// Interaction logic for NewItemWindow.xaml
-    /// </summary>
     public partial class NewItemWindow : Window
     {
+        NewItemWindowViewModel _viewModel = new NewItemWindowViewModel();
         public NewItemWindow()
         {
             InitializeComponent();
+            this.DataContext = _viewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Dish dish = new Dish
+                {
+                    Category = (Category)this.Category.SelectedItem,
+                    Name = this.Name.Text,
+                    Calories = float.Parse(this.Calories.Text),
+                    Proteins = float.Parse(this.Proteins.Text),
+                    Fats = float.Parse(this.Fats.Text),
+                    Carbohydrates = float.Parse(this.Carbohydrates.Text)
+                };
+                _viewModel.AddNewItem(dish);
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте правильность введённых данных");
+            }
         }
     }
 }
